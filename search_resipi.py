0cd1fc3eaecacaa740cb9a5de12303527511b48e
Tomoya-Hirano-04 <125986805+Tomoya-Hirano-04@users.noreply.github.com>
2023-04-23 12:02:50 +0900

    Add files via upload

--- search_resipi.py
+++ search_resipi.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import random
-from google.oauth2.service_account import ServiceAccountCredentials
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Google APIの認証情報を設定
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
